198669817aa35686b351aae12e8205e4fcebafe6
Richard Li <lichard49@gmail.com>
2021-04-21 16:03:21 -0700

    Load the web-based Google Keep in the chat head's WebView

--- app/src/main/java/com/lichard49/keeperhead/BubbleActivity.java
+++ app/src/main/java/com/lichard49/keeperhead/BubbleActivity.java
@@ -2,6 +2,8 @@ package com.lichard49.keeperhead;
 
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +13,17 @@ public class BubbleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubble);
+
+        WebView myWebView = findViewById(R.id.keepView);
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // prevent the native Keep app from launching
+                return false;
+            }
+        });
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        myWebView.loadUrl("https://keep.google.com/");
     }
 }
